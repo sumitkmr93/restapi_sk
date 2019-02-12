69f2dcaadb6eddc375dfad9be4b4c72daea67edb
Sumit Kumar <sumitkmr93@outlook.com>
2019-02-12 13:42:55 +0000

    added two tests

--- test/testrest.js
+++ test/testrest.js
@@ -37,3 +37,35 @@ describe('/POST add a contact', () => {
     });
 
 });
+
+describe('/PUT change a contact', () => {
+    it('PUT a contact', (done) => {
+        let contact = {
+            name: "Sumit K",
+            phone: "001298756767"
+        }
+        chai.request(server)
+          .put('/contacts/5c6186d19078b5148ca7740a')
+          .send(contact)
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name').eql('Sumit K')
+            done();
+          });
+    });
+
+});
+
+describe('/DELETE delete a contact', () => {
+    it('DELETE a contact', (done) => {
+        chai.request(server)
+          .delete('/contacts/5c6186d19078b5148ca7740a')
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+          });
+    });
+
+});
